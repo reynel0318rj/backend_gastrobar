c36a21cf8c10c2b0ada850697145422010ca4cfc
reynel0318rj <reynel0318@gmail.com>
2026-06-16 12:39:12 -0500

    creacion de enpoint para preguntar que roll tiene el usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const userRoutes = require('./userRoutes');
 const menuRoutes = require('./menuRoutes');
 const reservationRoutes = require('./reservationRoutes');
+const { protect } = require('../middleware/authMiddleware');
+const { getProfile } = require('../controllers/userController');
 
 const router = express.Router();
 
+router.get('/me', protect, getProfile);
 router.use('/users', userRoutes);
 router.use('/menus', menuRoutes);
 router.use('/reservations', reservationRoutes);
